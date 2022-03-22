7a6bd30a12e115b09f831efba85dd42c5a5b7fab
Ethan <23EmThomas@macon.k12.mo.us>
2022-03-22 10:03:44 -0500

    Started
    e

---
+++ oldGlory/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace oldGlory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
